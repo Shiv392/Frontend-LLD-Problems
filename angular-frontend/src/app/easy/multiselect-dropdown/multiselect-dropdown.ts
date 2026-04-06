6a1f4e130913a47ce6241adf1ee85256af429dd3
Shiv Soni <sonishiv309@gmail.com>
2026-04-06 16:03:44 +0530

    created components for data-table, model, multiselect-dropdown, toastnotificatio

---
+++ angular-frontend/src/app/easy/multiselect-dropdown/multiselect-dropdown.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-multiselect-dropdown',
+  imports: [],
+  templateUrl: './multiselect-dropdown.html',
+  styleUrl: './multiselect-dropdown.css',
+})
+export class MultiselectDropdown {}
